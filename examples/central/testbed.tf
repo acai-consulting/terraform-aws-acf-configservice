947b2d7317c617cfca345799cd9708d62074536f
Michael Ullrich <micahel.ullrich@acai.gmbh>
2024-09-09 08:33:46 +0200

    fix

--- examples/central/testbed.tf
+++ examples/central/testbed.tf
@@ -34,6 +34,14 @@ provider "aws" {
 }
 
 provider "aws" {
+  region = "us-east-2"
+  alias  = "workload_use2"
+  assume_role {
+    role_arn = "arn:aws:iam::767398146370:role/OrganizationAccountAccessRole" # ACAI AWS Testbed Workload Account
+  }
+}
+
+provider "aws" {
   region = "eu-central-1"
   assume_role {
     role_arn = "arn:aws:iam::767398146370:role/OrganizationAccountAccessRole" # ACAI AWS Testbed Workload Account
